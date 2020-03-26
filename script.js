9bb30f85ef33d613e9bc7ecfdf8999b4c3b8eafb
scadler <scadler@s.sfusd.edu>
2020-03-26 19:46:12 +0000

    movement works

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const user = {
     x : canvas.width/2,
     y : canvas.height-190,
     Vx : 0,
+    Vy : 0,
+    slow: 0.2,
 }
 const racer = {
     x : 100,
@@ -48,6 +50,7 @@ function drawText(text,x, y, color){
 }
 function update(){
     user.x += user.Vx
+    user.y += user.Vy + user.slow
     if(user.x < 125){
         user.Vx = 0
         user.x = 126
@@ -56,10 +59,18 @@ function update(){
         user.Vx = 0
         user.x = anvas.width-176
     }
+    else if(user.y > canvas.height-115){
+        user.slow = 0
+        user.y = canvas.height-116
+    }
+    else if(user.y < 25){
+        user.y = 26;
+    }
 
 }
 function render(){
-drawRect(0, 0, canvas.width, canvas.height, "#666666");
+drawRect(0, 0, canvas.width, canvas.height, "#90EE90");
+drawRect(100, 0, 400, canvas.height, "#666666");
 drawUser(user.x, user.y)
 }
 function game(){
@@ -76,6 +87,13 @@ function keyPressed(e){
     }
     else if(key == "d"){
         user.Vx = 0.5
+    }
+    else if (key == "w") {
+        user.Vy = -0.5
+        user.slow = 0.2
+    }
+    else if (key == "s") {
+        user.Vy = 0
     }
     else if(key == " ") {
     e.preventDefault();
